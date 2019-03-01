5d2d26b8ab1caefa0b4ec6b711fbf5d065bb809e
kattya espinoza <kattya.espinoza@jalasoft.com>
2019-03-01 11:44:37 -0400

    Added Integration Controller
    Added main file

---
+++ src/com/jalasoft/shopping_car/controller/controller.py
@@ -0,0 +1,6 @@
+# Controller
+
+class Controller:
+    def __init__(self, view, model):
+        print("intUI")
+        view.init_ui()
---
+++ src/com/jalasoft/shopping_car/ui/view.py
@@ -0,0 +1,6 @@
+class View:
+    def __init__(self):
+        print("view")
+
+    def init_ui(self):
+        print()
