70ee8462f7aa4f976145ea10aee9f8f595d09037
pipo411 <ariel.gonzales@fundacion-jala.org>
2019-03-18 00:40:38 -0400

    Added cursor on database_manager

--- src/com/jalasoft/shopping_car/db/database_manager.py
+++ src/com/jalasoft/shopping_car/db/database_manager.py
@@ -13,11 +13,12 @@ class DatabaseManager:
 
     def update_quantity_field(self, quantity, id):
         self.db_connection.execute("""UPDATE items SET QUANTITY = ? WHERE  ID = ?""", (quantity, id))
+        self.db_connection.commit()
 
     def get_items_as_dictionary(self):
-        items = self.db_connection.execute("select ID,NAME,PRICE,QUANTITY from items")
+        cursor = self.db_connection.cursor()
+        items = cursor.execute("select ID,NAME,PRICE,QUANTITY from items")
         dict_of_items = {}
         for row in items:
             dict_of_items[row[1]] = {"id": row[0], "name": row[1], "price": row[2], "quantity": row[3]}
         return dict_of_items
-
