545ed58981edaab359015b0fd9b4e61b2733320f
vzamorano <vivianzamoranocastelo58@gmail.com>
2019-03-29 23:17:44 -0400

    final updates

--- src/com/jalasoft/shopping_car/db/database_manager.py
+++ src/com/jalasoft/shopping_car/db/database_manager.py
@@ -1,12 +1,15 @@
+"""
+This class has the methods with the db queries.
+"""
 import logging
 import time
 
 from src.com.jalasoft.shopping_car.db.database_connection import DatabaseConnection
-from src.com.jalasoft.shopping_car.logger import LOG
 from src.com.jalasoft.shopping_car.model.item import Item
 
+LOG = logging.getLogger()
 
-time = (time.strftime("%d/%m/%y"))
+TIME = (time.strftime("%d/%m/%y"))
 
 
 class DatabaseManager:
@@ -16,23 +19,42 @@ class DatabaseManager:
         self.db_connection = self.db
 
     def insert_element(self, item):
-        LOG.debug("Insert Item")
-        LOG.debug(item.get_name())
+        """
+        This method inserts elements on db.
+        :param item: Item()
+        """
+        LOG.info("Insert Item")
+        LOG.info(item.get_name())
         self.db_connection.execute("INSERT into items (NAME,PRICE,QUANTITY) values (?,?,?)",
                                    (item.get_name(), item.get_price(), item.get_quantity()))
         self.db_connection.commit()
-        LOG.debug("Complete insert item")
+        LOG.info("Complete insert item")
 
     def delete_element(self, id):
+        """
+        This method deletes elements on db.
+        :param id: id
+        """
         self.db_connection.execute("""DELETE FROM items WHERE  ID = ?""", (id,))
         self.db_connection.commit()
 
     def update_quantity_field(self, quantity, id):
+        """
+        This method updates the quantity
+        :param quantity: quantity
+        :param id: id
+        """
         self.db_connection.execute("""UPDATE items SET QUANTITY = ? WHERE  ID = ?""",
                                    (quantity, id))
         self.db_connection.commit()
 
     def update_item_description(self, id, name=None, price=None):
+        """
+        This method updates the name and price of table items
+        :param id: id
+        :param name: str name
+        :param price: price
+        """
         if name is not None:
             self.db_connection.execute("""UPDATE items SET NAME = ? WHERE  ID = ?""",
                                        (name, id))
@@ -42,6 +64,10 @@ class DatabaseManager:
         self.db_connection.commit()
 
     def get_items_as_dictionary(self):
+        """
+        This method gets items of item table
+        :return: dict_of_items
+        """
         cursor = self.db_connection.cursor()
         items = cursor.execute("select ID,NAME,PRICE,QUANTITY from items WHERE QUANTITY  > 0")
         dict_of_items = {}
@@ -55,13 +81,31 @@ class DatabaseManager:
             dict_of_items[row[1]] = item
         return dict_of_items
 
+    def get_item(self, name):
+        """
+        This method gets items for name
+        :param name: str name
+        :return: Item()
+        """
+        cursor = self.db_connection.cursor()
+        item = cursor.execute("select ID,NAME,PRICE,QUANTITY from items WHERE NAME  =  ?", (name))
+        return item
+
     def insert_element_records(self, product):
+        """
+        This method inserts elements on records table
+        :param product: product
+        """
         print("Insert ", product.get_name())
         self.db_connection.execute("INSERT into records (NAME,PRICE,QUANTITY,PURCHASE_DATE) values (?,?,?,?)",
-                                   (product.get_name(), product.get_price(), product.get_quantity(), time))
+                                   (product.get_name(), product.get_price(), product.get_quantity(), TIME))
         self.db_connection.commit()
 
     def get_records_as_list(self):
+        """
+        This method gets records as list of records table
+        :return: list_of_records
+        """
         cursor = self.db_connection.cursor()
         records = cursor.execute("select ID,NAME,PRICE,QUANTITY,PURCHASE_DATE from records  WHERE QUANTITY  > 0")
         list_of_records = []
@@ -74,6 +118,6 @@ class DatabaseManager:
 # oreo = Item("oreo", 17.5, 9)
 # db.insert_element_records(oreo)
 # print(db.get_records_as_list())
-# db.delete_element(id)
+# db.delete_element(1)
 # db.insert_element_records(oreo)
 # print(db.get_records_as_list())
